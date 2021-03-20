96716825218377e1bdb8e7f8ea105208a875ecd2
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-03-20 13:45:47 +0000

    Some changes
    - Organized into folders
    - Added diagnostics script (not finished)
    - Added imperfect testing (w/o resampling) model notebook

---
+++ python/diagnostics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pystan
+import arviz as arvz
+
+
+class SimulatedData:
+    def __init__(self, type):
+        self.model = model    
+    
+    
+class Diagnostic:
+    
+    def __init__(self, model):
+        self.model = model    
+    
+    def runtime_lineplot(self, N_list):
+        # Plots model runtime for varying fitting data sample sizes
+        
+    def runtime_lineplot(self, P_list):    
+        # Plots runtime for varying number of parameters
+        
+    def convergence_lineplot:
+        # Plots runtime as a function of the number of thetas to estimate
+        
+    def convergence_lineplot:
+        # Plots runtime as a function of the number of thetas to estimate    
+        
+    def mse_lineplot:
+        # Plots runtime as a function of the number of thetas to estimate
+        
+        
+    
