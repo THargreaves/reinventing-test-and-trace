0ad91a24952970d2b13a89ba11609f683157d550
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-03-20 18:16:53 +0000

    Update diagnostic_tools.py

--- python/model-diagnostics/diagnostic_tools.py
+++ python/model-diagnostics/diagnostic_tools.py
@@ -8,12 +8,18 @@ np.random.seed(SEED)
 # Note: To comply with upcoming version of PyStan syntax (without model_name attribute) 
 # Define Diagnotic object using Model object and str name
 
-class Diagnostic:
+class Posetrior:
+    """
+    Posterior for base model
     
-    def __init__(self, model, name):
+    :param N: fiting sample size
+    :param P: number of settings
+    """
+    def __init__(self, model, N, P):
         self.model = model
-        self.name = name
-    
+        self.N = N
+        self.P = P
+        
     def simulate_ground_truth(self, P):
         
         # Simulating transmission and occurence rates
@@ -21,18 +27,12 @@ class Diagnostic:
         occurrence_rate = np.random.beta(2, 10, P)
         base_rate = np.random.beta(2, 10, 1)
         
-        # Simulating testing rates
-        t_i = np.random.beta(8, 2, 1)  # Prob(tested | infected)
-        t_not_i = np.random.beta(2, 20, 1)  # Prob(tested | not-infected)
-        testing_rates = np.array([t_i, t_not_i])
-        
-        # Simulating test sensitivity and specificity rates
-        test_sensitivity = np.random.beta(4, 3, 1)  # True positive rate
-        test_specificity = np.random.beta(50, 2, 1)  # True negative rate
-        test_accuracy_rates = np.array([test_sensitivity, test_specificity])
-        return {'true_theta':transmission_rate, 'true_rho':base_rate, 'true_gamma':testing_rates, 'true_lambda':testing_rates}
-        
-    def simulate_data(self, N, P, ground_truth):
+        self.ground_truth = {'true_occurence_rate':occurrence_rate, 
+                            'true_theta':transmission_rate, 
+                            'true_rho':base_rate}
+    
+    def simulate_data(self):
+        # Using
         # Simulating data
         data = {}
         for p in range(P):
@@ -50,42 +50,12 @@ class Diagnostic:
             X = X.loc[:, X.columns.str.startswith('O')]
             return {'N': N, 'P': P, 'X': X.to_numpy(), 'y': y.to_numpy()}
         
-        elif self.name == 'tt_imperfect_testing':
-            # Introducing false positives and negatives
-            y = y*np.random.binomial(1, true_lambda[0], N) + (1-y)*np.random.binomial(1, (1-true_lambda[1]), N)
-            X = X.loc[:, X.columns.str.startswith('O')]
-            return {'N':N, 'P':P, 'X':X.to_numpy(), 'y':y.to_numpy(), 'mean_lambda':mean_rates, 'se_lambda':se_rates}
-
-        elif self.name == 'tt_resampling': 
-            # Resampling using testing probabilites conditional on infected
-            tested = y*np.random.binomial(1, true_gamma[0], N) + (1-y)*np.random.binomial(1, true_gamma[1], N)
-            y = y[tested == 1]
-            X_survey = X[tested == 0].reset_index()
-            X = X[tested == 1].reset_index()
-
-            N = X.shape[0]
-            NA = X_survey.shape[0]
-
-            X = X.loc[:, X.columns.str.startswith('O')]
-            X_survey = X_survey.loc[:, X_survey.columns.str.startswith('O')]
-            return {'N':N, 'NA':NA, 'P':P, 'X':X.to_numpy(), 'y':y.to_numpy(), 'survey':X_survey.to_numpy()}
-            
-        elif self.name == 'tt_single_lvl_final':
-            # Resampling using testing probabilites conditional on infected
-            tested = y*np.random.binomial(1, true_gamma[0], N) + (1-y)*np.random.binomial(1, true_gamma[1], N)
-            y = y[tested == 1]
-            X_survey = X[tested == 0].reset_index()
-            X = X[tested == 1].reset_index()
-            
-            N = X.shape[0]
-            NA = X_survey.shape[0]
-            
-            # Introducing false positives and negatives
-            y = y*np.random.binomial(1, true_lambda[0], N) + (1-y)*np.random.binomial(1, (1-true_lambda[1]), N)
-            
-            X = X.loc[:, X.columns.str.startswith('O')]
-            X_survey = X_survey.loc[:, X_survey.columns.str.startswith('O')] 
-            return {'N':N, 'NA':NA, 'P':P, 'X':X.to_numpy(), 'y':y.to_numpy(), 'survey':X_survey.to_numpy()}
+class Diagnostic:
+    
+    def __init__(self, model, name):
+        self.model = model
+        self.name = name
+        
         
     def runtime_lineplot(self, N_space):
         # Plots model runtime as function of fitting sample size
