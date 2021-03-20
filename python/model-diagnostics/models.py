59f97f13a1d72d1ad50a1ae717d225098b436d18
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-03-20 18:35:45 +0000

    Update models.py

--- python/model-diagnostics/models.py
+++ python/model-diagnostics/models.py
@@ -4,54 +4,19 @@ import matplotlib.pyplot as plt
 import pystan
 import arviz as arvz
 
-
-# Base model
-base_model = pystan.StanModel(model_name='base_model', model_code="""
-data {
-  int<lower=0> N;                            // number of observations
-  int<lower=0> P;                            // number of places
-  int<lower=0, upper=1> X[N,P];              // activity occurrences
-  int<lower=0, upper=1> y[N];                // transmission (tested positive)
-  
-}
-parameters {
-  real<lower=0, upper=1> theta[P];           // transmission rates
-  real<lower=0, upper=1> rho;                // underlying risk
-}
-model {
-  // Precomputation
-  real log1m_theta[P];
-  real log1m_rho;
-  
-  for (p in 1:P) {
-    log1m_theta[p] = log1m(theta[p]);
-  }
-
-  log1m_rho = log1m(rho);
-    
-  // Priors
-  theta ~ uniform(0, 1);
-  rho ~ uniform(0, 1);
-  
-  // Likelihood
-  for (n in 1:N) {
-    real s = 0.0;
-    for (p in 1:P) {
-      if (X[n,p] == 1) {
-        s += log1m_theta[p];
-      }
-    }
-    s += log1m_rho;
-    
-    if (y[n] == 1) {
-      target += log1m_exp(s);
-    } 
-    else {
-      target += s;
-    }
-  }
-}
-""")
+class BaseModel:
+    def __init__(self, model,N,P):
+        self.model = gen
+    def simulate_ground_truth(self, P):
+        
+        # Simulating transmission and occurence rates
+        transmission_rate = np.random.beta(2, 10, P)
+        occurrence_rate = np.random.beta(2, 10, P)
+        base_rate = np.random.beta(2, 10, 1)
+        
+        self.ground_truth = {'true_occurence_rate':occurrence_rate, 
+                            'true_theta':transmission_rate, 
+                            'true_rho':base_rate}
 
 
         
