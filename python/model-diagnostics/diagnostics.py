44384133868f90b08fe47e5f0edc3dbeafd0f4ec
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-03-20 15:45:43 +0000

    Update diagnostics

--- python/model-diagnostics/diagnostics.py
+++ python/model-diagnostics/diagnostics.py
@@ -28,21 +28,41 @@ class Diagnostic:
         test_specificity = np.random.beta(50, 2, 1)  # True negative rate
         true_lambda = np.array([test_sensitivity, test_specificity])
         
-    def simulate_sample(self, N, P):
-        # Simulate fitting sample
+    def simulate_data(self, N, P):
+        # Simulate data
         data = {}
         for p in range(P):
             occurrence = np.random.binomial(1, true_occurrence_rate[p], N)
             transmission = occurrence * np.random.binomial(1, true_transmission_rate[p], N)
             data[f'O{p+1}'] = occurrence
             data[f'T{p+1}'] = transmission
-
+        
         data['T0'] = np.random.binomial(1, base_rate, N)
         X = pd.DataFrame(data)
         z = X.loc[:, X.columns.str.startswith('T')].sum(axis=1)
         y = (z > 0).astype(int)
+        
+        if self.model.model_name == 'base_model':
+            X = X.loc[:, X.columns.str.startswith('O')]
+            return {'N': N, 'P': P, 'X': X.to_numpy(), 'y': y.to_numpy()}
+
+        elif:
+        # Resampling using testing probabilites conditional on infected
+        tested = y*np.random.binomial(1, true_gamma[0], N) + (1-y)*np.random.binomial(1, true_gamma[1], N)
+        y = y[tested == 1]
+        X_survey = X[tested == 0].reset_index()
+        X = X[tested == 1].reset_index()
+        
+        N = X.shape[0]
+        NA = X_survey.shape[0]
+        
+        # Introducing false positives and negatives
+        y = y*np.random.binomial(1, true_lambda[0], N) + (1-y)*np.random.binomial(1, (1-true_lambda[1]), N)
+        
         X = X.loc[:, X.columns.str.startswith('O')]
-        if ()
+        X_survey = X_survey.loc[:, X_survey.columns.str.startswith('O')]
+        X.info()
+        X_survey.info()
         
 
     def runtime_lineplot(self, N_list):
